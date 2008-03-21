d8359618105f97d0a1c03d27c331c5ad083a7ce8
mh <mh@immerda.ch>
2008-03-21 18:06:39 +0000

    added a webhosting class to manage /srv, heavily changed pure-ftpd module to fit better our needs, tryout with parameter giving

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,73 +5,58 @@
 # modules_dir { "pureftp": }
 
 class pureftp {
-    $modulename = "pureftp" 
-    $pkgname = "pure-ftpd"
-    $gentoocat = "net-ftp"
-    $cnfname = "pure-ftpd"
-    $cnfpath = "/etc/conf.d"
+    case $operatingsystem {
+        gentoo: { include pure-ftpd::gentoo }
+        default: { include pure-ftpd::base }
+    }
+}
 
-    package { $pkgname:
+class pureftp::base {
+    package { pure-ftpd:
         ensure => present,
-        category => $operatingsystem ? {
-            gentoo => $gentoocat,
-            default => '',
-        }
+    }
+
+    file{"/srv/ftp":
+        ensure => directory,
+        owner => root,
+        uid => 0,
+        mode => 755,
+        require => File[/srv],
     }
 
     file{
-        "${cnfpath}/${cnfname}":
+        "/srv/ftp/pureftpd.passwd":
             source => [
-                "puppet://$server/dist/${modulename}/${fqdn}/${cnfname}",
-                "puppet://$server/${modulename}/${fqdn}/${cnfname}",
-                "puppet://$server/${modulename}/${cnfname}"
+                "puppet://$server/dist/pureftp/${fqdn}/pureftpd.passwd",
+                "puppet://$server/dist/pureftp/${fqdn}/pureftpd.passwd",
+                "puppet://$server/pureftp/pureftpd.passwd"
             ],
             owner => root,
             group => 0,
-            mode => 0444,
-            require => Package[$pkgname],
-            notify => Service[$pkgname],
+            mode => 600,
+            require => [ Package[pure-ftpd], File[/srv/ftp] ],
+            notify => Exec[update_pure-ftpd_db],
     }
 
-    # db
-    $file1name="pureftpd.pdb"
-    $file1path="/srv/ftp"
-    file{
-        "${file1path}/${$file1name}":
-            source => [
-                "puppet://$server/dist/${modulename}/${fqdn}/${file1name}",
-                "puppet://$server/${modulename}/${fqdn}/${file1name}",
-                "puppet://$server/${modulename}/${file1name}"
-            ],
-            owner => root,
-            group => 0,
-            mode => 0444,
-            require => Package[$pkgname],
-            notify => Service[$pkgname],
-    }
-
-    $file2name="pureftpd.passwd"
-    $file2path="/srv/ftp"
-    file{
-        "${file2path}/${$file2name}":
-            source => [
-                "puppet://$server/dist/${modulename}/${fqdn}/${file2name}",
-                "puppet://$server/${modulename}/${fqdn}/${file2name}",
-                "puppet://$server/${modulename}/${file2name}"
-            ],
-            owner => root,
-            group => 0,
-            mode => 0444,
-            require => Package[$pkgname],
+    exec{update_pure-ftpd_db:
+        command => "pure-pw mkdb /srv/ftp/pureftpd.pdb -f /srv/ftp/pureftpd.passwd",
+        path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin",
+        refreshonly => true,
     }
 
     service { 
-        $pkgname: 
+        pure-ftpd: 
             ensure  => running,
             enable => true,
             hasstatus => true,
             hasrestart => true,
     } 
+}
 
+class pureftpd::gentoo inherits pureftpd::base {
+    Package[pure-ftpd]{
+        category => 'net-ftp'
+    }
 
+    gentoo::etcconfd { pure-ftpd: require => Package[pure-ftpd], notify => Service[pure-ftpd]}
 }
